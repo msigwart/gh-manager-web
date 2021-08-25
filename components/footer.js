de9cdca903811a80b0b0d70fc822a20ca3541160
Marten Sigwart <marten@okamaja.de>
2021-08-25 12:29:19 +0200

    add logo in footer

--- components/footer.js
+++ components/footer.js
@@ -1,12 +1,18 @@
+import Image from 'next/image'
+
 export default function Footer() {
   return (
-    <footer className={`absolute bottom-0`}>
+    <footer className={`absolute bottom-0 py-3`}>
       <a
         href="https://datawrapper.de"
         target="_blank"
         rel="noopener noreferrer"
+        className="flex flex-row items-center gap-2 text-black relative"
       >
-        Powered by Datawrapper
+        Powered by
+        <div className="relative -bottom-1">
+          <Image src="/datawrapper.png" alt="Datawrapper Logo" width={118} height={32} />
+        </div>
       </a>
     </footer>
   )
